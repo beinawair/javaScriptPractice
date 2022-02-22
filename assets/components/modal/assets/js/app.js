1fad455ef2054ca6abdac51301dea5e2cc7bbc1a
beinawair <notifier.id@gmail.com>
2022-02-23 00:38:26 +0800

    project practice no. 06 - modal is completed

--- assets/components/modal/assets/js/app.js
+++ assets/components/modal/assets/js/app.js
@@ -1,1 +1,11 @@
-//on progress
+const modalOpn = document.querySelector('.modal-btn');
+const modalCls = document.querySelector('.close-btn');
+const modalOv = document.querySelector('.modal-overlay');
+
+modalOpn.addEventListener('click', function(){
+    modalOv.classList.add('open-modal');
+});
+
+modalCls.addEventListener('click', function(){
+    modalOv.classList.remove('open-modal');
+});
