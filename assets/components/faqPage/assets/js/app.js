65d716a6d198763b300d06ff3ec2fcb76c3fcc93
beinawair <notifier.id@gmail.com>
2022-02-26 07:29:07 +0800

    Project practice no.07 - FAQ Page completed

--- assets/components/faqPage/assets/js/app.js
+++ assets/components/faqPage/assets/js/app.js
@@ -1,1 +1,18 @@
-//on progress
+const text = document.querySelectorAll('.question');
+
+text.forEach(function(que){
+    const btn = que.querySelector('.question-btn');
+    const ans = que.querySelector('.answer');
+
+    btn.addEventListener('click', function(){
+
+        text.forEach(function(item){
+            if(item !== que) {
+                item.classList.remove('active');
+            }
+        });
+        que.classList.toggle('active');
+        btn.classList.toggle('active');
+        ans.classList.toggle('active');
+    });
+});
